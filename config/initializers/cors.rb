729ca8356284b83b50e42f767bf7e58585218f84
big-meel <big.meel@zoho.com>
2022-03-13 19:27:48 -0500

    Update cors config

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
 # Initialize CORS at start
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV["UI_ORIGIN"]
-    resource ENV["UI"], headers: :any, methods: [:get, :post, :put, :patch, :delete, :options]
+    origins '*'
+    resource 'api/*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options]
   end
 end
